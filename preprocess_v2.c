1dc0f3e651823d64dbf267de35744e2c7f585050
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-29 15:22:01 +0000

    Support C11 string.h declarations

--- preprocess_v2.c
+++ preprocess_v2.c
@@ -259,6 +259,34 @@ static char *get_builtin_header(char *name) {
                "#define offsetof(type, member) __builtin_offsetof(type, member)\n"
                "#endif\n";
     }
+    if (!strcmp(name, "string.h")) {
+        return "#ifndef __MINICC_STRING_H\n"
+               "#define __MINICC_STRING_H 1\n"
+               "#include <stddef.h>\n"
+               "void *memcpy(void * restrict s1, const void * restrict s2, size_t n);\n"
+               "void *memmove(void *s1, const void *s2, size_t n);\n"
+               "char *strcpy(char * restrict s1, const char * restrict s2);\n"
+               "char *strncpy(char * restrict s1, const char * restrict s2, size_t n);\n"
+               "char *strcat(char * restrict s1, const char * restrict s2);\n"
+               "char *strncat(char * restrict s1, const char * restrict s2, size_t n);\n"
+               "int memcmp(const void *s1, const void *s2, size_t n);\n"
+               "int strcmp(const char *s1, const char *s2);\n"
+               "int strcoll(const char *s1, const char *s2);\n"
+               "int strncmp(const char *s1, const char *s2, size_t n);\n"
+               "size_t strxfrm(char * restrict s1, const char * restrict s2, size_t n);\n"
+               "void *memchr(const void *s, int c, size_t n);\n"
+               "char *strchr(const char *s, int c);\n"
+               "size_t strcspn(const char *s1, const char *s2);\n"
+               "char *strpbrk(const char *s1, const char *s2);\n"
+               "char *strrchr(const char *s, int c);\n"
+               "size_t strspn(const char *s1, const char *s2);\n"
+               "char *strstr(const char *s1, const char *s2);\n"
+               "char *strtok(char * restrict s1, const char * restrict s2);\n"
+               "void *memset(void *s, int c, size_t n);\n"
+               "char *strerror(int errnum);\n"
+               "size_t strlen(const char *s);\n"
+               "#endif\n";
+    }
     if (!strcmp(name, "limits.h")) {
         return "#define CHAR_BIT 8\n"
                "#define SCHAR_MIN (-127 - 1)\n"
