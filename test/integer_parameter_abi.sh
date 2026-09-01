db7147e3b3a909743f06fe1a9b9bb3b2bce17d94
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-01 16:40:54 +0800

    Test _Bool return ABI interoperability

--- test/integer_parameter_abi.sh
+++ test/integer_parameter_abi.sh
@@ -26,6 +26,14 @@ int mini_bool_regs(_Bool a, _Bool b, _Bool c) {
 int mini_bool_stack(int a, int b, int c, int d, int e, int f, _Bool g) {
   return a == 1 && b == 2 && c == 3 && d == 4 && e == 5 && f == 6 && g == 1;
 }
+
+_Bool mini_bool_return_int(int x) {
+  return x;
+}
+
+_Bool mini_bool_return_double(double x) {
+  return x;
+}
 EOF
 ./minicc tmp-intparam-mini-callee.c > tmp-intparam-mini-callee.s
 cc -c -o tmp-intparam-mini-callee.o tmp-intparam-mini-callee.s
@@ -35,6 +43,8 @@ int mini_regs(signed char, unsigned char, short, unsigned short);
 int mini_stack(int, int, int, int, int, int, signed char, unsigned short);
 int mini_bool_regs(_Bool, _Bool, _Bool);
 int mini_bool_stack(int, int, int, int, int, int, _Bool);
+_Bool mini_bool_return_int(int);
+_Bool mini_bool_return_double(double);
 
 int main(void) {
   if (!mini_regs(-7, 250, -300, 65000))
@@ -45,6 +55,10 @@ int main(void) {
     return 3;
   if (!mini_bool_stack(1, 2, 3, 4, 5, 6, -9))
     return 4;
+  if (mini_bool_return_int(0) != 0 || mini_bool_return_int(-7) != 1)
+    return 5;
+  if (mini_bool_return_double(0.0) != 0 || mini_bool_return_double(2.5) != 1)
+    return 6;
   return 0;
 }
 EOF
@@ -77,6 +91,14 @@ int host_bool_regs(_Bool a, _Bool b, _Bool c) {
 int host_bool_stack(int a, int b, int c, int d, int e, int f, _Bool g) {
   return a == 1 && b == 2 && c == 3 && d == 4 && e == 5 && f == 6 && g == 1;
 }
+
+_Bool host_bool_return_int(int x) {
+  return x;
+}
+
+_Bool host_bool_return_double(double x) {
+  return x;
+}
 EOF
 cc -c -o tmp-intparam-host-callee.o tmp-intparam-host-callee.c
 
@@ -85,10 +107,13 @@ int host_regs(signed char, unsigned char, short, unsigned short);
 int host_stack(int, int, int, int, int, int, signed char, unsigned short);
 int host_bool_regs(_Bool, _Bool, _Bool);
 int host_bool_stack(int, int, int, int, int, int, _Bool);
+_Bool host_bool_return_int(int);
+_Bool host_bool_return_double(double);
 
 int main(void) {
   int (*fp)(int, int, int, int, int, int, signed char, unsigned short) = host_stack;
   int (*boolfp)(int, int, int, int, int, int, _Bool) = host_bool_stack;
+  _Bool (*retfp)(int) = host_bool_return_int;
   if (!host_regs(-7, 506, -300, 131071))
     return 1;
   if (!fp(1, 2, 3, 4, 5, 6, -9, -1))
@@ -97,6 +122,10 @@ int main(void) {
     return 3;
   if (!boolfp(1, 2, 3, 4, 5, 6, 99))
     return 4;
+  if (retfp(0) != 0 || retfp(-7) != 1)
+    return 5;
+  if (host_bool_return_double(0.0) != 0 || host_bool_return_double(-2.5) != 1)
+    return 6;
   return 0;
 }
 EOF
@@ -106,4 +135,4 @@ cc -o tmp-intparam-mini-to-host tmp-intparam-mini-caller.s tmp-intparam-host-cal
 
 echo 'OK(integer parameter ABI): minicc caller -> host callee'
 
-echo 'All narrow integer and _Bool fixed-parameter ABI tests passed!'
+echo 'All narrow integer and _Bool fixed-parameter/return ABI tests passed!'
